39a3eaa19371d1f4a44756ef97f0a59d595b8767
froger-me <alex@froger.me>
2018-04-21 14:20:41 +0800

    Update and rename unpublish.php to wp-unpublish.php

--- wp-unpublish.php
+++ wp-unpublish.php
@@ -1,6 +1,6 @@
 <?php
 /**
-Plugin Name: Unpublish
+Plugin Name: WP-Unpublish
 Plugin URI: https://truerun.com/
 Description: Adds the post status Unpublished.
 Version: 1.0
@@ -14,15 +14,15 @@ WC tested up to: 3.3.4
 if ( ! defined( 'ABSPATH' ) ) {
 	exit; // Exit if accessed directly
 }
-if ( ! defined( 'UNPUBLISH_PLUGIN_PATH' ) ) {
-	define( 'UNPUBLISH_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
+if ( ! defined( 'WP_UNPUBLISH_PLUGIN_PATH' ) ) {
+	define( 'WP_UNPUBLISH_PLUGIN_PATH', plugin_dir_path( __FILE__ ) );
 }
-if ( ! defined( 'UNPUBLISH_PLUGIN_URL' ) ) {
-	define( 'UNPUBLISH_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+if ( ! defined( 'WP_UNPUBLISH_PLUGIN_URL' ) ) {
+	define( 'WP_UNPUBLISH_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 }
 
 function wp_unpublish_run() {
-	require_once UNPUBLISH_PLUGIN_PATH . '/inc/class-wp-unpublish.php';
+	require_once WP_UNPUBLISH_PLUGIN_PATH . '/inc/class-wp-unpublish.php';
 
 	$unpublish = new WP_Unpublish();
 }
